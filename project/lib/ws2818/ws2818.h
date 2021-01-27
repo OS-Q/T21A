77391ff19ebb7131550ad7cb87c1a282d485f9b0
qitas <qitas@qitas.cn>
2021-01-27 19:10:11 +0800

    add

---
+++ project/lib/ws2818/ws2818.h
@@ -0,0 +1,15 @@
+#ifndef __ws2818_H
+#define __ws2818_H
+
+#define BIT_HIGH (45 * 2 / 3)
+#define BIT_LOW (45 / 3)
+#define RESET (0)
+
+#define LED_NUMBER          8
+#define LED_DATA_SIZE       (LED_NUMBER * 24)
+
+#define RESET_PERIOD        50
+
+
+
+#endif
